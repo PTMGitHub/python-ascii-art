90c427e73bf55379ebc5e87bfc1fc88b36200eb0
Paul Marsh <61422343+PTMGitHub@users.noreply.github.com>
2020-08-24 11:13:59 +0100

    Finished set 2

--- README.md
+++ README.md
@@ -52,3 +52,44 @@ This was the code to get the pixel info into a 2d array
     pixels = [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
 ```
 I had to look at his completed source code.  I need to unpack this so i know exactly whats going on - job for next week
+
+**Commited to GitHub**
+
+So what i believe is happening:
+```python
+img.getdata()  
+```
+
+This brings back the pixel info we want but it returned as an object
+
+```python
+pixel = list(img.getdata())  
+```
+You can cast it to a list but this is a flat list so need to convert it to a 2d list 
+```python
+pixels = [    pixels[i:i+img.width] for i in range(0, len(pixels),          img.width)]
+'''
+This makes it into a 2d list, each pix line is always gonnabe the same lenght (img.width). so this then loops throught and 'cuts' the long list of pixs everytime it his the width value.
+
+### End Result
+```python
+from PIL import Image
+try:
+    #Reads in the image and prints a message saying so and a messages printing the image size
+    img = Image.open("./lion2.png",mode='r')
+    print('Succesfully loaded image!')
+    print("Image size: " + str(img.width) + " x " + str(img.height))
+
+    #gets the pixel info and cast it to a list. this is flat
+    pixels = list(img.getdata())
+    #makes the list into a 2d list.
+    pixels = [    pixels[i:i+img.width] for i in range(0, len(pixels),          img.width)]
+
+
+    #img.show()
+    
+except:
+    print("Image loading didnt work")
+```
+
+**Commited to GitHub**
--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 from PIL import Image
 try:
     #Reads in the image and prints a message saying so and a messages printing the image size
-    img = Image.open("./ascii-pineapple.jpg",mode='r')
+    img = Image.open("./lion2.png",mode='r')
     print('Succesfully loaded image!')
     print("Image size: " + str(img.width) + " x " + str(img.height))
 
+    #gets the pixel info and cast it to a list. this is flat
     pixels = list(img.getdata())
-  #  pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
-    pixels = [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
+    #makes the list into a 2d list.
+    pixels = [    pixels[i:i+img.width] for i in range(0, len(pixels),          img.width)]
 
-    print(pixels[0][0])
-    print(len(pixels))
-    # print('\n')
-    # print(img_px_matrix[0])
-    # print(img_px_matrix[0][0])
-    # print(len(img_px_matrix))
-    # print(len(img_px_matrix[0]))
-    # print('\n')
 
     #img.show()
     
-    # pix_2d_list = []
-    # print("Here's the image pixel info:")
-    # import pdb; pdb.set_trace()
-    # for x in img_px_matrix:
-    #     pix_2d_list.append(img_px_matrix[x])
-
-    # print(pix_2d_list)
-
-    # for x in img_px_matrix:
-    # #    print('hi')
-    #     import pdb; pdb.set_trace()
-    #     for y in len(img_px_matrix[x]):
-    #         print('hello')
-    # #         pixel = img_px_matrix[x][y]
-    # #         print(pixel)
-
-    # print(img_px_matrix)
-  
 except:
     print("Image loading didnt work")
